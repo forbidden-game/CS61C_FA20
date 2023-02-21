59b73ed8cc098df7f84d8010b27d9c310b791af3
forbiddengame <panxiezhao@gmail.com>
2023-02-21 09:16:25 +0800

    lfsr v1

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lfsr.h"
+#include "bit_ops.h"
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+	unsigned bit_0 = get_bit(*reg, 0);
+	unsigned bit_2 = get_bit(*reg, 2);
+	unsigned bit_3 = get_bit(*reg, 3);
+	unsigned bit_5 = get_bit(*reg, 5);
+	unsigned bit_15 = bit_0 ^ bit_2 ^ bit_3 ^ bit_5;
+	*reg >>= 1;
+	set_bit(reg, 15, bit_15);
 }
 
