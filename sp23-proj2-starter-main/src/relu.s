3f9eea85c8ea03df5fbb8cb17f869710cd9c594c
forbiddengame <panxiezhao@gmail.com>
2023-02-28 12:12:49 +0800

    sp-23-proj2-partA-ReLU

--- sp23-proj2-starter-main/src/relu.s
+++ sp23-proj2-starter-main/src/relu.s
@@ -14,26 +14,38 @@
 # ==============================================================================
 relu:
     # Prologue
+    addi sp sp -4
+    sw s0 0(sp)
+    # END Prologue
 
-		b
+    li t0 1
+    li t1 0
+    bge a1 t0 loop_start
+    li a0 36
+    j exit
+    
 loop_start:
-
-
-
-
-
-
-
-
-
+    bge t1 a1 loop_end
+    slli t2 t1 2
+    add t2 a0 t2
+    lw s0 0(t2)
+    blt s0 x0 loop_continue
+    addi t1 t1 1
+    j loop_start
+    
 loop_continue:
-
-
-
+    sw x0 0(t2)
+    addi t1 t1 1
+    j loop_start
+    
 loop_end:
-
+    li a0 17
+    li a1 0
+    ecall
 
     # Epilogue
-
-
+    lw s0 0(sp)
+    addi sp sp 4
+    # END Epilogue
+    
     jr ra
